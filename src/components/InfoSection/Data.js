baa6670798a21643c0ee0d8da562d383faa33878
Zainal Abidin <53314006+zaiinhs@users.noreply.github.com>
2020-10-21 00:20:04 +0700

    third commit

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headLine: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'dicover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Premium Bank',
+    headLine: 'Unlimited Access',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg'),
+    alt: 'Car',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headLine: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-3.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
